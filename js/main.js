1cb26363d7a6cedc7a05dd82d43cb52156df36c6
thomasjodt <jodt5200@gmail.com>
2022-04-27 16:45:47 -0500

    Added new Favicon, Logo and improved the footer section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const burger = document.querySelector('.burger-button');
 const navList = document.querySelector('.nav-right');
+const linkList = document.getElementsByClassName('nav-links');
+links = Array.from(linkList)
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     navList.classList.toggle('active');
+})
+links.forEach(e => {
+    e.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        navList.classList.toggle('active');
+    })
 })
