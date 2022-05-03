ee149dcdfe7ed75fa9e4e87f589e13e4f1c730de
thomasjodt <jodt5200@gmail.com>
2022-05-02 23:04:34 -0500

    Minimized logo's weight, changed the banner on the header, added a favicon and added some styles to the navbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const burger = document.querySelector('.burger-button');
 const navList = document.querySelector('.nav-right');
-const linkList = document.getElementsByClassName('nav-links');
-links = Array.from(linkList)
+const links = Array.from(document.getElementsByClassName('nav-links'))
 
 burger.addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('active')
     burger.classList.toggle('active');
     navList.classList.toggle('active');
 })
@@ -11,5 +11,6 @@ links.forEach(e => {
     e.addEventListener('click', () => {
         burger.classList.toggle('active');
         navList.classList.toggle('active');
+        document.querySelector('nav').classList.toggle('active')
     })
 })
