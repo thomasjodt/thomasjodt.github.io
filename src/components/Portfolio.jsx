9a365b6bc0bbb1561f5b352867cd691990257224
thomasjodt <jodt5200@gmail.com>
2022-05-10 07:52:35 -0500

    [UPDATE] Added more little changes for the view on large viewport

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,14 +6,16 @@ export default function Portfolio() {
   return (
     <section id='Portfolio' >
       <h2 className='portfolio-title' >Projects</h2>
-
+      <section className="projects-layout">
+        
       {projects.map((project) => (
         <Project
-          url={project.url}
-          image={project.image}
-          name={project.name} />
-      ))}
+        url={project.url}
+        image={project.image}
+        name={project.name} />
+        ))}
 
+        </section>
     </section>
   )
 }
